f67312be5a016bafe4677ea691a95881f5694d27
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-13 15:33:44 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,24 @@
+curl https://sdk.cloud.google.com | bash
+python -m pip install discord.py[voice]
+python -m pip install numpy
+python -m pip install logger
+python -m pip install oauth2client
+python -m pip install pynacl
+python -m pip install pydub
+python -m pip install PyNaCl
+python -m pip install python-dotenv
+python -m pip install structlog
+python -m pip install PyNaCl library
+python -m pip install python-dotenv
+python -m pip install Flask
+python -m pip install google-api-python-client
+python -m pip install google-auth-httplib2
+python -m pip install google-auth-oauthlib
+python -m pip install google-cloud-storage
+python -m pip install google-cloud-texttospeech
+python -m pip install --upgrade google-api-python-client
+python -m pip install --upgrade google-cloud-storage
+python -m pip install --upgrade google-cloud-texttospeech
+python -m pip install --upgrade pip
+python -m pip install --upgrade pydata-google-auth
+sudo dnf install ffmpeg
