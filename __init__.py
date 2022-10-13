c9c9c2a54b63fd7bf0f0b68079884148808223e6
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-13 15:50:32 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 curl https://sdk.cloud.google.com | bash
-python -m pip install discord.py[voice]
+python -m pip install discord[voice]
 python -m pip install numpy
 python -m pip install logger
 python -m pip install oauth2client
