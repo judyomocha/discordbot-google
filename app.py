881521eb9652ad49075c21697ede49c5237c251a
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-18 10:49:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,45 +9,13 @@ TOKEN = os.environ['TOKEN']
 CHANNEL_ID = os.environ['CHANNEL_ID']
 SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
 SPREADSHEET_NAME = os.environ['SPREADSHEET_NAME']
-YOUR_PROJECT_ID = os.environ['YOUR_PROJECT_ID']
-YOUR_SECRET_ID = os.environ['YOUR_SECRET_ID']
-
-# Import the Secret Manager client library.
-from google.cloud import secretmanager
-# GCP project in which to store secrets in Secret Manager.
-project_id = "YOUR_PROJECT_ID"
-# ID of the secret to create.
-secret_id = "YOUR_SECRET_ID"
-# Create the Secret Manager client.
-client = secretmanager.SecretManagerServiceClient()
-# Build the parent name from the project.
-parent = f"projects/{project_id}"
-# Create the parent secret.
-secret = client.create_secret(
-    request={
-        "parent": parent,
-        "secret_id": secret_id,
-        "secret": {"replication": {"automatic": {}}},
-    }
-)
-# Add the secret version.
-version = client.add_secret_version(
-    request={"parent": secret.name, "payload": {"data": b"hello world!"}}
-)
-# Access the secret version.
-response = client.access_secret_version(request={"name": version.name})
-# Print the secret payload.
-#
-# WARNING: Do not print the secret in a production environment - this
-# snippet is showing how to access the secret material.
-payload = response.payload.data.decode("UTF-8")
-print("Plaintext: {}".format(payload))
+NAME = os.environ['NAME']
 
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name(payload, scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(NAME, scope)
 
 def last(SPREADSHEET_KEY,SPREADSHEET_NAME):
     gs = gspread.authorize(credentials)
